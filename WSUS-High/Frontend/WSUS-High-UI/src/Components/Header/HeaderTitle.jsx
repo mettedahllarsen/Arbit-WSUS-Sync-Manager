56222405b0f7c43b6e46c41f9afb587ac160f16c
miksezea <mikkel.eilskov@gmail.com>
2024-05-07 12:26:48 +0200

    - Npm update - Connection to API is now working - Changed from TSX to JSX

---
+++ WSUS-High/Frontend/WSUS-High-UI/src/Components/Header/HeaderTitle.jsx
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+
+const HeaderTitle = () => {
+  useEffect(() => {
+    console.log("Component HeaderTitle mounted");
+  }, []);
+
+  return (
+    <div className="text-start ms-4">
+      <h5 className="text-dark">
+        <strong>WSUS HIGH UI</strong>
+      </h5>
+
+      <span className="text-dark">
+        <strong>Hostname: </strong>
+      </span>
+
+      <span className="text-secondary">
+        <strong>{window.location.hostname}</strong>
+      </span>
+    </div>
+  );
+};
+
+export default HeaderTitle;
---
+++ WSUS-High/Frontend/WSUS-High-UI/src/Components/Modals/ConfigurationsModal.jsx
@@ -0,0 +1,13 @@
+import PropTypes from "prop-types";
+import { Modal } from "react-bootstrap";
+
+const ConfigurationsModal = (props) => {
+  return <Modal show={props.show} onHide={() => props.hide()}></Modal>;
+};
+
+ConfigurationsModal.propTypes = {
+  show: PropTypes.bool.isRequired,
+  hide: PropTypes.func.isRequired,
+};
+
+export default ConfigurationsModal;
